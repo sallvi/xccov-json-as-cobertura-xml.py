68399133b1fa22e3fe3e44ef50690373563f5097
Salvatore Fabio Rocca <salvatore.f.rocca@gmail.com>
2020-01-01 18:39:46 +0100

    Clean up script. Add samples.

--- xccov-json-as-cobertura-xml.py
+++ xccov-json-as-cobertura-xml.py
@@ -37,23 +37,16 @@ def get_xml_data(json_data, xcode_project_directory):
     element_coverage.set('version', '0.1')
     element_coverage.set('timestamp', str(int(time.time())))
     
-    add_xml_element_sources(element_coverage)
-    add_xml_element_packages(element_coverage, xcode_project_directory, json_data)
-    
-    return element_coverage
-    
-def add_xml_element_sources(parent_element):
-    element_sources = ElementTree.SubElement(parent_element, 'sources')
+    element_sources = ElementTree.SubElement(element_coverage, 'sources')
     for source in [os.getcwd(), os.path.realpath(__file__)]:
         element_source = ElementTree.SubElement(element_sources, 'source')
         element_source.text = source
-        
-def add_xml_element_packages(parent_element, xcode_project_directory, json_data):
-    element_packages = ElementTree.SubElement(parent_element, 'packages')
+    
+    element_packages = ElementTree.SubElement(element_coverage, 'packages')
     for target in json_data['targets']:
         current_package_name = None
         current_package_coverage = None
-        files = sorted(target['files'], key=lambda file: file['path'])
+        files = sorted(target['files'], key=lambda x: x['path'])
         for file in files:
             class_filename = file['path'].replace(os.getcwd() + "/", "") # Relative path to file
             
@@ -73,58 +66,30 @@ def add_xml_element_packages(parent_element, xcode_project_directory, json_data)
             current_package_coverage = (current_package_coverage[0] + 1, current_package_coverage[1] + file['lineCoverage'])
             element_package.set('line-rate', str((current_package_coverage[1] / current_package_coverage[0])))
             
-            add_xml_element_class(element_classes, class_filename, file)
-        
-def add_xml_element_packagesbak(parent_element, xcode_project_directory, json_data):
-    element_packages = ElementTree.SubElement(parent_element, 'packages')
-    for target in json_data['targets']:
-        current_package_name = None
-        current_package_coverage = None
-        files = sorted(target['files'], key=lambda file: file['path'])
-        for file in files:
-            class_filename = file['path'].replace(os.getcwd() + "/", "") # Relative path to file
+            element_class = ElementTree.SubElement(element_classes, 'class')
+            element_class.set('name', os.path.splitext(os.path.basename(file['name']))[0])
+            element_class.set('filename', class_filename)
+            element_class.set('line-rate', str(file['lineCoverage']))
+            element_class.set('branch-rate', '1.0')
+            element_class.set('complexity', '0.0')
             
-            package_name = target['name'] + "." + (os.path.split(class_filename)[0]).replace("/", ".")
-            if xcode_project_directory is not None and class_filename.startswith(xcode_project_directory):
-                package_name = package_name.replace(xcode_project_directory + ".", "", 1)
-            if package_name != current_package_name:
-                element_package = ElementTree.SubElement(element_packages, 'package')
-                element_package.set('name', package_name)
-                element_package.set('branch-rate', '1.0')
-                element_package.set('complexity', '0.0')
-                element_classes = ElementTree.SubElement(element_package, 'classes')
-                
-                current_package_name = package_name
-                current_package_coverage = (0, 0)
+            element_methods = ElementTree.SubElement(element_class, 'methods')
             
-            current_package_coverage = (current_package_coverage[0] + 1, current_package_coverage[1] + file['lineCoverage'])
-            element_package.set('line-rate', str((current_package_coverage[1] / current_package_coverage[0])))
-            
-            add_xml_element_class(element_classes, class_filename, file)
-            
-def add_xml_element_class(parent_element, filename, json_data):
-    element_class = ElementTree.SubElement(parent_element, 'class')
-    element_class.set('name', os.path.splitext(os.path.basename(json_data['name']))[0])
-    element_class.set('filename', filename)
-    element_class.set('line-rate', str(json_data['lineCoverage']))
-    element_class.set('branch-rate', '1.0')
-    element_class.set('complexity', '0.0')
+            lines = {} # Contains a key for every number with the value being the execution count
+            for function in file['functions']:
+                for line in range(0, function['executableLines']):
+                    number = function['lineNumber'] + line
+                    hits = function['executionCount'] if line < function['coveredLines'] else 0
+                    lines[number] = min(lines.get(number, hits), hits) # In case of duplicate line numbers, the lower execution count value is being used (this leads to a more realistic overall coverage report in Cobertura when handling completion blocks within functions)
 
-    element_methods = ElementTree.SubElement(element_class, 'methods')
-
-    lines = {} # Contains a key for every number with the value being the execution count
-    for function in json_data['functions']:
-        for line in range(0, function['executableLines']):
-            number = function['lineNumber'] + line
-            hits = function['executionCount'] if line < function['coveredLines'] else 0
-            lines[number] = min(lines.get(number, hits), hits) # In case of duplicate line numbers, the lower execution count value is being used (this leads to a more realistic overall coverage report in Cobertura when handling completion blocks within functions)
-
-    element_lines = ElementTree.SubElement(element_class, 'lines')
-    for number, hits in lines.items():
-        element_line = ElementTree.SubElement(element_lines, 'line')
-        element_line.set('number', str(number))
-        element_line.set('hits', str(hits))
-        element_line.set('branch', "false")
+            element_lines = ElementTree.SubElement(element_class, 'lines')
+            for number, hits in lines.items():
+                element_line = ElementTree.SubElement(element_lines, 'line')
+                element_line.set('number', str(number))
+                element_line.set('hits', str(hits))
+                element_line.set('branch', "false")
+    
+    return element_coverage
 
 ###
 ### XML: Printing
